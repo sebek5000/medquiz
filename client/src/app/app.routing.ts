97f8b92c7a6259195f544910110991cfd326d5a9
Sebastian Latka <sebastian.latka@capgemini.com>
2018-12-05 18:00:33 +0100

    add route to questions list so that others (Kamil) could see it clearly

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import {QuestionListComponent} from './question-list/question-list.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent },
+    {path: 'question-list', component: QuestionListComponent},
     { path: '**', redirectTo: '' }
 ];
 
