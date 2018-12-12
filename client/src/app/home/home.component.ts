f6ecb7b561469722ff0f9141f37b57d4fadd8c3f
magdalenabiernat <magdalena.biernat@poczta.fm>
2018-12-12 12:02:03 +0100

    change and add .css and views

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -7,7 +7,8 @@ import 'rxjs/add/operator/map';
 
 @Component({
     selector:'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
